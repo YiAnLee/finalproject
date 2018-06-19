93cca9ead9bb4889918df2a9371a2ca313be45a5
twetto <franky85@hotmail.com.tw>
2018-06-19 22:33:41 +0800

    emlab6

---
+++ emlab6/src/run.py
@@ -0,0 +1,37 @@
+# Author : Yi-Chun Hung
+# Email : nick831111@gmail.com
+# Date : 05/28/2018
+# Usage : Run the trained model in PC.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import cv2
+from keras.models import load_model
+from scipy.misc import imresize
+
+label = {0: 'neg', 1: 'pos'}
+# If you train multiple classes, you may change the index to your label.
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Load the trained model and run.')
+    parser.add_argument("--model_path", required=True,
+                        help="The path to the model file.", type=str)
+    parser.add_argument("--target_img_size", required=True, nargs='*',
+                        help="The target image size to the model.", type=int)
+    args = parser.parse_args()
+
+    if len(args.target_img_size) != 2:
+        raise Exception('length of traget_img_size should be 2')
+
+    cap = cv2.VideoCapture(1)
+    model = load_model(args.model_path)
+
+    for _ in range(60):
+        ret, frame = cap.read()
+        frame_resize = imresize(frame, tuple(args.target_img_size))
+        # [possibility_neg, possibility_pos]
+        ans = model.predict(np.expand_dims(frame_resize, axis=0))
+        ans = ans.argmax(axis=-1)
+        print(label[ans[0]])
